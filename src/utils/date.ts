cf84ac16cad8e5ac5d280d5fde9cfd08f0a43b46
Kristóf Poduszló <14854048+kripod@users.noreply.github.com>
2024-04-16 13:04:29 +0200

    fix(stringifyDate): invalid and extended date handling

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,13 +1,4 @@
-function padMonthOrDay(value: number) {
-  return value.toString().padStart(2, "0");
-}
-
 export function stringifyDate(date: Date) {
-  return (
-    date.getUTCFullYear() +
-    "-" +
-    padMonthOrDay(date.getUTCMonth() + 1) +
-    "-" +
-    padMonthOrDay(date.getUTCDate())
-  );
+  const dateTime = date.toISOString();
+  return dateTime.slice(0, dateTime.indexOf("T"));
 }
