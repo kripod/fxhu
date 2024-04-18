ccbce4a91dd6aa196c001039c2ee6da1fa27ce3c
Kristóf Poduszló <14854048+kripod@users.noreply.github.com>
2024-04-19 01:57:55 +0200

    refactor: make 'openapi.json' static

--- src/pages/api/v1/openapi.json.ts
+++ src/pages/api/v1/openapi.json.ts
@@ -1,40 +1,12 @@
 import type { APIRoute } from "astro";
-import type { OpenAPIV3_1 } from "openapi-types";
 
-const OAS_VERSION = "3.1.0";
+import data from "./_openapi.json";
 
-export const mediaType = "application/openapi+json;version=" + OAS_VERSION;
+export const mediaType = "application/openapi+json;version=" + data.openapi;
 
-export const GET: APIRoute = (context) =>
-  Response.json(
-    {
-      openapi: OAS_VERSION,
-      info: {
-        title: "FXHU",
-        license: {
-          name: "MIT",
-          identifier: "MIT",
-        },
-        version: "1.0",
-      },
-      servers: [{ url: new URL("/api/v1", context.site).toString() }],
-      paths: {
-        "/symbols.json": {},
-        "/symbols/{symbol}.json": {},
-        "/symbols/{symbol}/{year}.json": {},
-        "/years.json": {},
-        "/years/{year}.json": {},
-      },
-      components: {
-        parameters: {},
-      },
-      externalDocs: {
-        url: context.site!.toString(),
-      },
-    } satisfies OpenAPIV3_1.Document,
-    {
-      headers: {
-        "content-type": mediaType,
-      },
+export const GET: APIRoute = () =>
+  Response.json(data, {
+    headers: {
+      "content-type": mediaType,
     },
-  );
+  });
