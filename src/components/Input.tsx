4ad0cebf2f53fe27db991abd1b4538a9356b50de
Kristóf Poduszló <14854048+kripod@users.noreply.github.com>
2024-05-23 05:44:12 +0200

    fix(Input): apply scoped reset

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,4 +1,5 @@
 import clsx from "clsx/lite";
+import { getResetClassName } from "css-homogenizer/reset-scoped";
 import { forwardRef } from "react";
 
 import type { Merge } from "../utils/types";
@@ -18,6 +19,7 @@ export const Input = forwardRef(function Input(
     <input
       ref={ref}
       className={clsx(
+        getResetClassName("input"),
         className,
         "border border-gray-300 hover:border-gray-700 dark:border-gray-700 dark:hover:border-gray-300",
         size === "sm" && "h-8 rounded-md px-2.5",
