77dd5b74b4598f120775a93e3a4db61d99ab009a
Kristóf Poduszló <14854048+kripod@users.noreply.github.com>
2024-12-15 05:45:35 +0100

    refactor(config): simplify collection names

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,17 +1,17 @@
 import { docsSchema } from "@astrojs/starlight/schema";
 import { defineCollection, z } from "astro:content";
 
-const docsCollection = defineCollection({
+const docs = defineCollection({
   type: "content",
   schema: docsSchema(),
 });
 
-const quoteRecordsCollection = defineCollection({
+const quoteRecords = defineCollection({
   type: "data",
   schema: z.record(z.string(), z.number()),
 });
 
 export const collections = {
-  docs: docsCollection,
-  quoteRecords: quoteRecordsCollection,
+  docs,
+  quoteRecords,
 };
