78c64ab544b149d9a82f3f5ecfad87a47fb6e520
Kristóf Poduszló <14854048+kripod@users.noreply.github.com>
2024-12-15 22:46:22 +0100

    refactor(isDateStale): name magic constant

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,10 +1,12 @@
 import { z } from "astro/zod";
 
+const AVG_DAYS_IN_MONTH = 30;
+
 export const ISODateSchema = z.string().date();
 
 export function isDateStale(date: Date) {
   const pivot = new Date();
-  pivot.setUTCDate(pivot.getUTCDate() - 30);
+  pivot.setUTCDate(pivot.getUTCDate() - AVG_DAYS_IN_MONTH);
   return date < pivot;
 }
 
