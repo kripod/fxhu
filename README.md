29024f98ed6432977c8ecc2f381e1257a6ca87c8
Kristóf Poduszló <14854048+kripod@users.noreply.github.com>
2024-12-15 21:58:37 +0100

    fix: move 'data/' to 'content/'

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ Exchange rates API sourced from the National Bank of Hungary (Magyar Nemzeti Ban
 
 The project is powered by [Astro](https://astro.build/) and hosted on [Cloudflare Pages](https://pages.cloudflare.com/).
 
-Data gets scraped regularly using [this GitHub Actions workflow](./.github/workflows/scraper.yaml). Static file endpoints are generated from [the resulting files](./src/data/tickers/) and then served through a CDN.
+Data gets scraped regularly using [this GitHub Actions workflow](./.github/workflows/scraper.yaml). Static file endpoints are generated from [the resulting files](./src/content/tickers/) and then served through a CDN.
 
 Documentation is built with [Starlight](https://starlight.astro.build/), leveraging [an OpenAPI renderer plugin](https://github.com/HiDeoo/starlight-openapi).
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,22 +1,17 @@
+import { docsLoader } from "@astrojs/starlight/loaders";
 import { docsSchema } from "@astrojs/starlight/schema";
 import { glob } from "astro/loaders";
 import { defineCollection } from "astro:content";
 
-import { TickerSchema } from "./data/tickers/_schema";
+import { TickerSchema } from "./content/tickers/_schema";
 
 const docs = defineCollection({
-  loader:
-    // TODO: Use `docsLoader()` from "@astrojs/starlight/loaders"
-    // https://github.com/withastro/starlight/issues/2698
-    glob({
-      pattern: "**/[^_]*.{markdown,mdown,mkdn,mkd,mdwn,md,mdx}",
-      base: "./src/data/docs",
-    }),
+  loader: docsLoader(),
   schema: docsSchema(),
 });
 
 const tickers = defineCollection({
-  loader: glob({ pattern: "[^_]*.json", base: "./src/data/tickers" }),
+  loader: glob({ pattern: "[^_]*.json", base: "./src/content/tickers" }),
   schema: TickerSchema,
 });
 
